86749ab9aaf2f721d3bd5ad9accce78352cc2d80
Tanadon Inmano <139216952+FramNaVer@users.noreply.github.com>
2026-05-30 22:48:02 +0700

    feat: set up authentication routes with register and login endpoints; integrate Prisma and PostgreSQL

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
+import authRouter from "./src/presentation/routes/auth.route";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
+
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
